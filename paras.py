21b1c9d4bf4fd731932759ae694196d06805db3b
Titan-Codes <vedansh1sharma2005@gmail.com>
2023-11-05 20:03:23 +0530

    added results page

---
+++ paras.py
@@ -0,0 +1,12 @@
+paragraphs = [
+    "The sun rose over the horizon, casting a warm, golden glow across the tranquil meadow. Birds chirped in the trees, and a gentle breeze rustled the leaves. It was a perfect morning for a leisurely walk in the countryside.",
+    "In a bustling city, people hurried along the crowded streets, each with their own destination in mind. Taxis honked, and the hum of conversation filled the air as life in the urban jungle carried on without pause.",
+    "The ancient castle stood atop the hill, its imposing stone walls a testament to centuries of history. Tourists marveled at the architecture and wondered about the stories hidden within the fortress's walls.",
+    "As the chef prepared the meal, the kitchen buzzed with activity. The sizzle of food in the pan, the clinking of utensils, and the aromatic scents filled the room with an atmosphere of culinary delight.",
+    "The serene lake stretched out before us, its glassy surface reflecting the surrounding mountains. The air was crisp, and the only sounds were the occasional call of a distant bird and the lapping of water against the shore.",
+    "The library was a haven of knowledge, with shelves upon shelves of books waiting to be explored. It was a place where one could lose themselves in stories and information, a refuge for the curious mind.",
+    "The courtroom was filled with tension as the lawyers presented their cases. The judge, in a solemn black robe, listened attentively as the fate of the accused hung in the balance.",
+    "The waves crashed against the rocky cliffs, sending salty spray into the air. It was a rugged and wild coastline, a place of rugged beauty that inspired awe in all who visited.",
+    "The factory floor was a hive of activity, with workers moving in synchronized chaos to assemble the intricate machinery. The rhythmic clanging and whirring of the machines created a symphony of industry.",
+    "In the heart of the forest, a hidden waterfall cascaded down moss-covered rocks. The sound of rushing water and the scent of earth and foliage made it a place of natural wonder and tranquility."
+]
